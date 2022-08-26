b0b7c08c3d566c1f70a7a205113d539340584263
tsunoda <tsunoda@aomori-u.ac.jp>
2022-08-26 16:03:04 +0900

    エラー修正

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
       builder: (context, snapshot) {
         if (!snapshot.hasData) return LinearProgressIndicator();
 
-        return _buildList(context, snapshot.data.docs);
+        return _buildList(context, snapshot.data!.docs);
       },
     );
   }
@@ -84,14 +84,14 @@ class Record {
   final int votes;
   final DocumentReference reference;
 
-  Record.fromMap(Map<String, dynamic> map)
+  Record.fromMap(Map<String, dynamic> map, {required this.reference})
       : assert(map['name'] != null),
         assert(map['votes'] != null),
         name = map['name'],
         votes = map['votes'];
 
   Record.fromSnapshot(DocumentSnapshot snapshot)
-      : this.fromMap(snapshot.data(), reference: snapshot.reference);
+      : this.fromMap(snapshot.data() as Map<String, dynamic>, reference: snapshot.reference);
 
   @override
   String toString() => "Record<$name:$votes>";
